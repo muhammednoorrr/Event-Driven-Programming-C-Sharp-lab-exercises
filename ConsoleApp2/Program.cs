b7bbaad7ebf0a5a1ea168344b789e47a687abffd
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-11 05:04:40 -1000

    Add files via upload
    Taking a string from the user and displaying the length without using the length property.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string userInput = Console.ReadLine();
+            
+
+            int length = CalculateStringLength(userInput);
+
+            Console.WriteLine($"Length of the string: {length}");
+            Console.ReadKey();
+        }
+        static int CalculateStringLength(string input)
+        {
+            int count = 0;
+
+            foreach (char c in input)
+            {
+                count++;
+            }
+
+            return count;
+        }
+        }
+}

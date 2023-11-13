5dfc7bd47a131a1da3822eebd3679670d404c0f6
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-12 21:51:13 -1000

    Add files via upload
    Taking a string from the user and counting the number of vowels and consonants.

---
+++ Q4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            int vowelCount = 0;
+            int consonantCount = 0;
+
+            foreach (char c in input.ToLower())
+            {
+                if (char.IsLetter(c))
+                {
+                    if ("aeiou".Contains(c))
+                    {
+                        vowelCount++;
+                    }
+                    else
+                    {
+                        consonantCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Vowels: {vowelCount}");
+            Console.WriteLine($"Consonants: {consonantCount}");
+            Console.ReadKey();
+        }
+    }
+}

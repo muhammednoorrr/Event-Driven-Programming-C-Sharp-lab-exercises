954901dd1ff30ff92abbe710db1aa186049ebbe6
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-12 21:38:10 -1000

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a String");
+            String stri= Console.ReadLine();
+
+            int Leng = stri.Length;
+            String newstr="";
+            for(int i = Leng - 1; i >=0; i--) {
+                newstr += stri[i];
+            }
+           Console.WriteLine(newstr);
+            Console.ReadKey();
+
+        }
+    }
+}

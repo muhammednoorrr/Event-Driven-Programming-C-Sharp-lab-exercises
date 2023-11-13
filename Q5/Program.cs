0093183793cc2f857d284e0cb08cb61f97bcaf8c
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-12 21:56:20 -1000

    Add files via upload
    Skyline Writing: Take a string from user and display it in skyline manner where every other letter is Uppercase.
    Example:- Hello-> HeLlO
                     welcome -> WeLcOmE

---
+++ Q5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            string skylineString = SkylineWriting(input);
+
+            Console.WriteLine("Skyline Writing: " + skylineString);
+            Console.ReadKey();
+        }
+
+        static string SkylineWriting(string input)
+        {
+            char[] charArray = input.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i += 2)
+            {
+                charArray[i] = char.ToUpper(charArray[i]);
+            }
+            return new string(charArray);
+
+        }
+    }
+}

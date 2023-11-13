7281f4edfd7fc0e871a9f710020141c935f4a448
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-12 21:48:09 -1000

    Add files via upload
    Taking a string from the user and displaying how many digits, alphabets and special characters it has.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            int digitCount = 0;
+            int alphabetCount = 0;
+            int specialCharCount = 0;
+
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    digitCount++;
+                }
+                else if (char.IsLetter(c))
+                {
+                    alphabetCount++;
+                }
+                else
+                {
+                    specialCharCount++;
+                }
+            }
+
+            Console.WriteLine($"Digits: {digitCount}");
+            Console.WriteLine($"Alphabets: {alphabetCount}");
+            Console.WriteLine($"Special Characters: {specialCharCount}");
+        }
+    }
+}
